0470d3346c5a70baa9d90dd2b9d7a2ce508fd740
DEV-COM <48807773+dev-com2020@users.noreply.github.com>
2024-03-24 12:13:39 +0100

    Update firebase-config.js

--- react-firebase/src/firebase-config.js
+++ react-firebase/src/firebase-config.js
@@ -2,12 +2,7 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "@firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHZEbmTZcSnkHRfHM89t_QjIGNDF7l9u8",
-  authDomain: "reactfirebase-249c6.firebaseapp.com",
-  projectId: "reactfirebase-249c6",
-  storageBucket: "reactfirebase-249c6.appspot.com",
-  messagingSenderId: "227851027877",
-  appId: "1:227851027877:web:591b256423fa7759d0d44c"
+
 };
 
 const app = initializeApp(firebaseConfig);
