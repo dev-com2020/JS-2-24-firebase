d34bc672eb7acd191237a0799771e3a3beece6b9
DEV-COM <kwp@tlen.pl>
2024-03-24 14:03:28 +0100

    db

---
+++ bookshelf-app/server/db.js
@@ -0,0 +1,40 @@
+const path = require('path')
+
+const dbPath = path.resolve(__dirname, 'db/database.sqlite')
+
+const knex = require('knex')({
+    client: 'sqlite3',
+    connection: {
+        filename: dbPath,
+    },
+    useNullAsDefault: true
+})
+
+knex.schema.hasTable('books').then((exists) => {
+    if (!exists) {
+        return knex.schema.createTable('books', (table) => {
+            table.increments('id').primary()
+            table.string('author')
+            table.string('title')
+            table.string('pubDate')
+            table.integer('rating')
+        })
+        .then(()=>{
+            console.log('Table books create')
+        })
+        .catch((error) => {
+            console.log(`Jest jakiś błąd: ${error}`)
+        })
+    }
+}).then(() => {
+    console.log('done')
+})
+.catch((error) => {
+    console.log(`Jest jakiś błąd ${error}`)
+})
+
+knex.select('*').from('books')
+.then(data => console.log('data',data))
+.catch(err => console.log(err))
+
+module.exports = knex
